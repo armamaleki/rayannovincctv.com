f547a80afad911e2b1f9bf1ad0042149507df912
arma <arma.malekii@gmail.com>
2025-10-01 13:16:57 +0330

    first push in server

--- resources/views/livewire/client/add-product-to-cart.blade.php
+++ resources/views/livewire/client/add-product-to-cart.blade.php
@@ -34,8 +34,6 @@
                 </button>
             </div>
         </div>
-        <button type="button" class="text-white">Cyan</button>
-
         <button class=" flex gap-2 items-center bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300  shadow-lg shadow-cyan-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
             <span>اضافه کردن به سبد خرید</span>
             <span wire:loading wire:target="addProduct">
